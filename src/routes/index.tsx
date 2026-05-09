826e4856c44a038e7dfa71027b33cef108bf6636
qin.dai <daiqin1046@gmail.com>
2026-05-09 23:41:50 +0800

    refactor: convert IndexRoute to a functional component with type annotation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import type { FC } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+
+const IndexRoute: FC = () => {
+  return <main aria-label="Application" />
+}
 
 export const Route = createFileRoute('/')({
   component: IndexRoute,
 })
-
-function IndexRoute() {
-  return <main aria-label="Application" />
-}
